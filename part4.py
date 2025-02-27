f58bd3102d56512af6d0448b5e08bcd6e7628ebd
Xu Yan <x.y.g.smits@student.vu.nl>
2025-02-27 14:27:18 +0100

    analysis and handling of missing values and duplicates

--- part4.py
+++ part4.py
@@ -1,20 +1,49 @@
 import pandas as pd
 import sqlite3
 
-# Read the complete.csv dataset
 df_complete = pd.read_csv('complete.csv')
+print(df_complete.head())
 
-# Check for missing values
 missing_values = df_complete.isnull().sum()
 print("Missing values in each column:\n", missing_values)
 
-# Resolve missing values (example: fill with 0 or forward fill)
-df_complete.fillna(0, inplace=True)
+# disregard the records containing missing values
+###df_complete = df_complete.dropna() # deletes all records that contain NaN
+###df_complete = df_complete.dropna(subset=['Confirmed', 'Deaths', 'Recovered']) # deletes records when these columns contain NaN
 
-# Check for duplicate entries
-duplicate_entries = df_complete.duplicated().sum()
-print("Number of duplicate entries:", duplicate_entries)
+# fill in 0 for missing values
+df_complete.fillna(
+    {col: 0 if df_complete[col].dtype in ['int64', 'float64'] else '' for col in df_complete.columns}, 
+    inplace=True
+) # fills in 0 for numeric columns and en empty string else
+###df_complete.fillna(0, inplace=True) # fills in 0 for all columns
+print(df_complete.head())
 
-# Remove duplicate entries
-df_complete.drop_duplicates(inplace=True)
+# fill in mean for missing values of numeric columns and 'Unkown' for string columns
+###df_complete.fillna({
+###    'Province.State': 'Unknown',
+###    'Confirmed': df_complete['Confirmed'].mean(),
+###    'Deaths': df_complete['Deaths'].mean(),
+###    'Recovered': df_complete['Recovered'].mean(),  
+###    'Active': df_complete['Active'].mean(), 
+###}, inplace=True)
+#print(df_complete.head())
 
+
+# fill in the previous value
+###df_complete.fillna(method='ffill', inplace=True)
+#print(df_complete.head())
+
+# fill in the next available value
+###df_complete.fillna(method='bfill', inplace=True)
+#print(df_complete.head())
+
+# check for duplicate entries
+duplicates = df_complete[df_complete.duplicated()] # all columns
+###duplicates = df_complete[df_complete.duplicated(subset=['Date', 'Country.Region', 'Confirmed'])] # subset of columns
+print(duplicates) # only prints the >2 occurence
+print(f"Total duplicate rows: {df_complete.duplicated().sum()}")
+
+# handle duplicate entries
+df_complete.drop_duplicates(inplace=True) # keep first occurence
+###df_complete = df_complete.drop_duplicates(subset=['Date', 'Country', 'Confirmed']) # delete duplicates based on a subset
